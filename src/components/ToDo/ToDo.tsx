e556229a1a038d2b4a104f52acd60939906365ea
Mattis <m.erkensten@icloud.com>
2021-10-27 16:46:12 +0200

    Git init

---
+++ src/components/ToDo/ToDo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ToDo.css'
+
+export const ToDo = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/ToDoPage/ToDoPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ToDoPage.css";
+
+export const ToDoPage = () => {
+  return <div></div>;
+};
